a5cdb68bdd1539cd2f2d31f2923c48ea5f20426e
José Ricardo <josericardosaraiva7@gmail.com>
2024-05-14 15:27:21 -0300

    feat: component Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,10 @@
-import {Text} from 'react-native';
-
 import { Container } from './styles';
+import { Header } from '@components/Header';
 
 export function Groups() {
   return (
     <Container>
-      <Text>groups</Text>
+      <Header />
     </Container>
   );
 }
