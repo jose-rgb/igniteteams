d699da9a0dd69142c4eab60beeaff56b2279276c
José Ricardo <josericardosaraiva7@gmail.com>
2024-05-17 21:26:59 -0300

    feat: componente PlayerCard & finalizando a interface

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
-import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 
 export function Players() {
@@ -47,6 +52,24 @@ export function Players() {
                 <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
             </HeaderList>
             
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <PlayerCard name={item} onRemove={() => {}}/>
+                )}
+                ListEmptyComponent={() => <ListEmpty message="Não há pessoas nesse time."/>}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    {paddingBottom: 80},
+                    players.length === 0 && { flex: 1 }
+                ]}
+            />
+
+            <Button
+                title="Remover Turma"
+                type="SECONDARY"
+            />
         </Container>
     );
 }
