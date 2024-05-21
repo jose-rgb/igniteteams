4e3143b52d50b2ad99be436ca8566135d3bddded
José Ricardo <josericardosaraiva7@gmail.com>
2024-05-21 19:24:32 -0300

    feat: tipando rotas do app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
-import { Highlight } from '@components/Highlight';
-import { Container} from './styles';
+import { useNavigation } from '@react-navigation/native';
+
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+import { Container} from './styles';
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -26,7 +35,7 @@ export function Groups() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Button title='Criar nova turma' />
+      <Button title='Criar nova turma' onPress={handleNewGroup} />
     </Container>
   );
 }
