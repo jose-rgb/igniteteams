6989a134dd05f36ee00193a0c6c00f4635bea6bb
José Ricardo <josericardosaraiva7@gmail.com>
2024-05-28 09:55:15 -0300

    feat: SafeAreaView

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components/native";
 import { CaretLeft } from "phosphor-react-native";
 
 export const Container = styled.View`
-    margin-top: 40px;
     width: 100%;
     flex-direction: row;
     align-items: center;
