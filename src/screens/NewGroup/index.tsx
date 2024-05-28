633e168819f6c580871934de8e4510e1f1c1a1ca
José Ricardo <josericardosaraiva7@gmail.com>
2024-05-28 09:38:46 -0300

    feat: butão voltar

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,18 @@
+import { useNavigation } from '@react-navigation/native';
 import { Highlight } from '@components/Highlight';
 import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
+
 export function NewGroup() {
+    const navigation = useNavigation();
+
+    function handleNew() {
+        navigation.navigate('players', {group: 'the'});
+    }
+
     return (
         <Container>
             <Header showBackButton/>
@@ -17,7 +25,7 @@ export function NewGroup() {
                     placeholder="Nome da turma"
                 />
 
-                <Button title="Criar"/>
+                <Button title="Criar" onPress={handleNew}/>
             </Content>
         </Container>
     );
