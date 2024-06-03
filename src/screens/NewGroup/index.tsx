c04ed58f6fe0bb6107577175ade0184faf3638f4
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-03 16:28:28 -0300

    feat: tratamento de erros

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { Alert } from 'react-native';
 
+import { AppError } from '@utils/AppError';
 import { groupCreate } from '@storage/group/groupCreate';
 
 import { Container, Content, Icon } from './styles';
@@ -11,6 +13,8 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
 
+
+
 export function NewGroup() {
     const [group, setGroup] = useState('');
     const navigation = useNavigation();
@@ -18,11 +22,20 @@ export function NewGroup() {
     async function handleNew() {
         try {
 
+            if(group.trim().length === 0){
+                return  Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+            }
+
             await groupCreate(group);
             navigation.navigate('players', {group: group});
 
         } catch (error) {
-          console.log(error)
+            if(error instanceof AppError){
+                Alert.alert('Novo Grupo', error.message);
+            } else {
+                Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+                console.log(error);
+            }
         }
     }
 
