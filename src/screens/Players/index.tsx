695f03074ae26e20e32189d1b4e1c394e0a7b732
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-04 18:12:32 -0300

    feat: armazenando players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import { Header } from '@components/Header';
@@ -12,6 +12,8 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+import { AppError } from '@utils/AppError';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
 
 type RouteParams = {
     group: string;
@@ -19,11 +21,36 @@ type RouteParams = {
 
 
 export function Players() {
+    const [newPlayerName, setNewPlayerName] = useState('');
     const [team, setTeam] = useState('Time A');
     const [players, setPlayers] = useState([]);
 
     const route = useRoute();
     const { group } = route.params as RouteParams;
+
+    async function handleAddPlayer() {
+        if(newPlayerName.trim().length === 0) {
+            return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.');
+        }
+
+        const newPlayer = {
+            name: newPlayerName,
+            team,
+        }
+
+        try {
+
+            await playerAddByGroup(newPlayer, group)
+
+        } catch (error) {
+            if(error instanceof AppError){
+                Alert.alert('Nova pessoa', error.message)
+            }else {
+                console.log(error);
+                Alert.alert('Nova pessoa', 'Não foi possível adicionar.')
+            }
+        }
+    }
 
     return(
         <Container>
@@ -36,11 +63,12 @@ export function Players() {
 
             <Form>
                 <Input 
+                    onChangeText={setNewPlayerName}
                     placeholder="Nome da pessoa"
                     autoCorrect={false}
                 />
 
-                <ButtonIcon icon="add"/>
+                <ButtonIcon icon="add" onPress={handleAddPlayer}/>
             </Form>
 
             <HeaderList>
---
+++ src/storage/player/PlayerStorageDTO.ts
@@ -0,0 +1,5 @@
+//tipagem que diz o que se guarda dos players
+export type PlayerStorageDTO = {
+    name: string;
+    team: string;
+}
