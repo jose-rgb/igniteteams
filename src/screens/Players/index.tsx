ba463ca5deff4cf811df366cfcd97bdaf4a828b8
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-05 10:48:45 -0300

    feat: removendo grupo

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { Alert, FlatList, Keyboard, TextInput } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import { useRoute, useNavigation } from '@react-navigation/native';
 
 import { AppError } from '@utils/AppError';
 
@@ -8,6 +8,7 @@ import { playerAddByGroup } from '@storage/player/playerAddByGroup';
 import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
 import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
 import { playerRemoveByGroup } from '@storage/player/playerRemoveByGroup';
+import { groupRemoveByName } from '@storage/group/groupRemoveByName';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -31,6 +32,7 @@ export function Players() {
     const [team, setTeam] = useState('Time A');
     const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
+    const navigation = useNavigation();
     const route = useRoute();
     const { group } = route.params as RouteParams;
 
@@ -94,6 +96,29 @@ export function Players() {
         }
     }
 
+    async function groupRemove() {
+        try {
+
+            await groupRemoveByName(group);
+            navigation.navigate('groups');
+
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Remover grupo', 'Não foi possível remover o grupo.');
+        }
+    }
+
+    async function handleGroupRemove() {
+        Alert.alert(
+            'Remover',
+            'Deseja remover o grupo?',
+            [
+                {text: 'Não'},
+                {text: 'Sim', onPress: () => groupRemove()},
+            ]
+        );
+    }
+
     return(
         <Container>
             <Header showBackButton/>
@@ -151,6 +176,7 @@ export function Players() {
             <Button
                 title="Remover Turma"
                 type="SECONDARY"
+                onPress={handleGroupRemove}
             />
         </Container>
     );
